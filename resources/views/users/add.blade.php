049219dc052f948c5962dc1afac77420a80a835b
Adriano <adrianoapi@hotmail.com>
2021-02-11 13:48:01 -0300

    add level in users

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -19,35 +19,30 @@
                         <div class="box-content nopadding">
                             <form action="#" method="POST" class='form-horizontal form-bordered'>
                             <div class="control-group">
-                                    <label for="nome" class="control-label">Nome completo</label>
+                                    <label for="name" class="control-label">Nome completo</label>
                                     <div class="controls">
-                                        <input type="text" name="nome" id="nome" placeholder="Text input" class="input-xlarge">
+                                        <input type="text" name="name" id="name" placeholder="Insira o nome" class="input-xlarge">
                                     </div>
                                 </div>
                                 <div class="control-group">
                                     <label for="email" class="control-label">E-mail</label>
                                     <div class="controls">
-                                        <input type="text" name="email" id="email" placeholder="Text input" class="input-xlarge">
-                                    </div>
-                                </div>
-                                <div class="control-group">
-                                    <label for="telefone" class="control-label">Telefone</label>
-                                    <div class="controls">
-                                        <input type="text" name="telefone" id="telefone" placeholder="Text input" class="input-xlarge">
+                                        <input type="text" name="email" id="email" placeholder="name@provider.domain" class="input-xlarge">
                                     </div>
                                 </div>
                                 <div class="control-group">
                                     <label for="password" class="control-label">Senha</label>
                                     <div class="controls">
-                                        <input type="password" name="password" id="password" placeholder="Password input" class="input-xlarge">
+                                        <input type="password" name="password" id="password" placeholder="Insira uma senha" class="input-xlarge">
                                     </div>
                                 </div>
                                 <div class="control-group">
-                                    <label for="textfield" class="control-label">Basic</label>
+                                    <label for="level" class="control-label">Nível</label>
                                     <div class="controls">
-                                        <select name="s2" id="s2" class='select2-me input-xlarge'>
-                                            <option value="01">Administrador</option>
-                                            <option value="02">Usuario</option>
+                                        <select name="level" id="level" class='select2-me input-xlarge'>
+                                            <option value="3">Proprietário</option>
+                                            <option value="2">Gerente</option>
+                                            <option value="1">Atendente</option>
                                         </select>
                                     </div>
                                 </div>
