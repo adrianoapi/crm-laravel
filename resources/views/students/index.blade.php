bc3be632aba1f6aa64487787d1307a86b5df44b0
Adriano <adrianoapi@hotmail.com>
2021-02-26 22:12:14 -0300

    add students pagination

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -77,6 +77,7 @@
                                 @endforeach
                                 </tbody>
                             </table>
+                            {{ $students->links('layouts.pagination') }}
                         </div>
 
                     </div>
