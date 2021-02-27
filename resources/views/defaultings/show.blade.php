feb937dc49747f7e800a8ad3ee87f4dcfabc679c
Adriano <adrianoapi@hotmail.com>
2021-02-27 08:11:59 -0300

    update defaultngs

--- resources/views/defaultings/show.blade.php
+++ resources/views/defaultings/show.blade.php
@@ -15,7 +15,7 @@
                 <div class="span12">
                     <div class="box box-bordered box-color">
                         <div class="box-title">
-                            <h3><i class="icon-th-list"></i> {{$title}}</h3>
+                            <h3><i class="icon-th-list"></i> {{$title}}: {{$defaulting->student->name}}</h3>
                         </div>
                         <div class="box-content nopadding">
                             <div class="tabs-container">
