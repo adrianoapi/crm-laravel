846ae15436ca166473b3cea37a6c557e093f63f3
Adriano <adrianoapi@hotmail.com>
2021-03-16 17:46:11 -0300

    fixed issue name space db

--- app/Http/Controllers/GraphicTradingController.php
+++ app/Http/Controllers/GraphicTradingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\GraphicTrading;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class GraphicTradingController extends Controller
 {
