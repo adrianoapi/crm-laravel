79a76a7ac7183a35104614d1e8771715ab26e595
Adriano <adrianoapi@hotmail.com>
2021-03-31 11:46:50 -0300

    remove columns to pdf in defaulting module

--- resources/views/defaultings/pdf.blade.php
+++ resources/views/defaultings/pdf.blade.php
@@ -12,8 +12,6 @@
             <th>Comercial</th>
             <th>Negociado</th>
             <th>Boleto</th>
-            <th>Total Líquido</th>
-            <th>Total Geral</th>
         </tr>
     </thead>
     <tbody>
@@ -29,17 +27,6 @@
             <td>{{$value->student->celular}}</td>
             <td>{{$value->student->comercial}}</td>
             <td><?php
-
-                $m_parcelas = $value->m_parcelas;
-                $m_parcela_pg = $value->m_parcela_pg;
-                $m_pendente = $value->m_parcelas - $value->m_parcela_pg;
-                $m_parcela_valor = $value->m_parcela_valor;
-
-                $s_parcelas = $value->s_parcelas;
-                $s_parcela_pg = $value->s_parcela_pg;
-                $s_pendente = $value->s_parcelas - $value->s_parcela_pg;
-                $s_parcela_valor = $value->s_parcela_valor;
-
                 if($value->negociado){
                     echo 'SIM';
                 }else{
@@ -53,30 +40,6 @@
                     echo 'NÃO';
                 }
             ?></td>
-            <td>{{$value->student->telefone}}/{{$value->student->celular}}</td>
-            <td>
-                <?php
-                    #Total Líquido
-                    $valor = str_replace(',', '.', str_replace('.', '', $s_parcela_valor));
-                    $s_valor_total = ($value->s_parcelas - $value->s_parcela_pg) * $valor;
-
-                    $multa = str_replace(',', '.', str_replace('.', '', $value->multa));
-                    $multa = $multa * $s_valor_total / 100;
-
-                    $valor = str_replace(',', '.', str_replace('.', '', $m_parcela_valor));
-                    $m_valor_total = ($m_parcelas - $m_parcela_pg) * $valor;
-
-                    $total = $m_valor_total + $multa;
-                    echo number_format($total, 2, ',', '.');
-                ?>
-            </td>
-            <td>
-                <?php
-                #Total Geral
-                $total = $m_valor_total + $s_valor_total;
-                echo number_format($total, 2, ',', '.');
-                ?>
-            </td>
         </tr>
     @endforeach
     </tbody>
