b6401fcdd079f0db981bff9a4995c1d358aa37c9
Adriano <adrianoapi@hotmail.com>
2021-04-08 15:21:33 -0300

    update navagation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,11 @@
 					<a href="#" class='toggle-subnav'><i class="icon-angle-down"></i><span>Financeiro</span></a>
 				</div>
 				<ul class="subnav-menu">
+                    @if(Auth::user()->level > 1)
+                    <li>
+                        <a href="{{route('caixa.index')}}">CAIXA</a>
+                    </li>
+                    @endif
 					<li>
 						<a href="{{route('defaultings.index')}}">CONTRATO - EVOLUTIME</a>
 					</li>
