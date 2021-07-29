8a442ab149fdb0dc5dc475f054ed6668dc3f3085
Adriano <adrianoapi@hotmail.com>
2021-07-29 19:15:13 -0300

    add filter in History index

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -20,9 +20,62 @@ class HistoryController extends Controller
     {
         $title = $this->title. " Dashboard";
 
+        $modulo = NULL;
+        $query  = NULL;
+
+        if(array_key_exists('modulo',$_GET))
+        {
+            if($_GET['modulo'] == 'cheque')
+            {
+                $modulo = 'cheque';
+                $query = "AND bc.id > 0";
+
+            }elseif($_GET['modulo'] == 'grafica'){
+                $modulo = 'grafica';
+                $query = "AND grt.id > 0";
+            }else{
+                $modulo = $_GET['modulo'];
+                if($modulo == 'contrato_segunda'){
+                    $query = "AND det.id > 0 AND de.fase = 'segunda'";
+                }else{
+                    $query = "AND det.id > 0 AND de.fase = 'terceira'";
+                }
+            }
+        }
+
+        $result = DB::select("SELECT de.fase, st.cod_unidade, st.cod_curso, st.ctr, st.name, st.cpf_cnpj,
+        if(bc.id > 0,bc.id, if(de.id > 0, de.id, gr.id)) AS modulo_id,
+        if(bc.id > 0,bct.id, if(de.id > 0, det.id, grt.id)) AS id,
+        if(bc.id > 0,'cheque', if(de.id > 0, 'contrato', 'grafica')) AS modulo,
+        if(bc.id > 0, bct.observacao, if(de.id > 0, det.observacao, grt.observacao)) AS observacao,
+        DATE_FORMAT(if(bc.id > 0, bct.created_at, if(de.id > 0, det.created_at, grt.created_at)), '%d/%m/%Y') AS created_at,
+        DATE_FORMAT(if(bc.id > 0, bct.dt_retorno, if(de.id > 0, det.dt_retorno, grt.dt_retorno)), '%d/%m/%Y %h:%i:%s') AS dt_retorno,
+        if(bc.id > 0, bct.schedule, if(de.id > 0, det.schedule, grt.schedule)) as schedule
+        FROM students AS st
+        LEFT JOIN bank_cheques AS bc
+               ON (st.id = bc.student_id)
+        LEFT JOIN bank_cheque_histories AS bct
+               ON (bc.id = bct.bank_cheque_id)
+        LEFT JOIN defaultings  AS de
+               ON (st.id = de.student_id)
+        LEFT JOIN defaulting_histories AS det
+               ON (de.id = det.defaulting_id)
+        LEFT JOIN graphics     AS gr
+               ON (st.id = gr.student_id)
+        LEFT JOIN graphic_histories AS grt
+               ON (gr.id = grt.graphic_id)
+        where
+        if(bc.id > 0, bct.schedule, if(de.id > 0, det.schedule, grt.schedule)) = 'open'
+        {$query}
+        order by if(bc.id > 0, bct.dt_retorno, if(de.id > 0, det.dt_retorno, grt.dt_retorno)) ASC
+        limit 30000"
+        );
+
+
         return view('historical.index', [
             'title' => $title,
-            'historical' => $this->getAll(),
+            'modulo' => $modulo,
+            'historical' => $result,
         ]);
     }
 
@@ -64,35 +117,6 @@ class HistoryController extends Controller
             die('Registro não encontrado!');
         }
 
-    }
-
-    public function getAll()
-    {
-        return DB::select("SELECT de.fase, st.cod_unidade, st.cod_curso, st.ctr, st.name, st.cpf_cnpj,
-        if(bc.id > 0,bc.id, if(de.id > 0, de.id, gr.id)) AS modulo_id,
-        if(bc.id > 0,bct.id, if(de.id > 0, det.id, grt.id)) AS id,
-        if(bc.id > 0,'cheque', if(de.id > 0, 'contrato', 'grafica')) AS modulo,
-        if(bc.id > 0, bct.observacao, if(de.id > 0, det.observacao, grt.observacao)) AS observacao,
-        DATE_FORMAT(if(bc.id > 0, bct.created_at, if(de.id > 0, det.created_at, grt.created_at)), '%d/%m/%Y') AS created_at,
-        if(bc.id > 0, bct.schedule, if(de.id > 0, det.schedule, grt.schedule)) as schedule
-        FROM students AS st
-        LEFT JOIN bank_cheques AS bc
-               ON (st.id = bc.student_id)
-        LEFT JOIN bank_cheque_histories AS bct
-               ON (bc.id = bct.bank_cheque_id)
-        LEFT JOIN defaultings  AS de
-               ON (st.id = de.student_id)
-        LEFT JOIN defaulting_histories AS det
-               ON (de.id = det.defaulting_id)
-        LEFT JOIN graphics     AS gr
-               ON (st.id = gr.student_id)
-        LEFT JOIN graphic_histories AS grt
-               ON (gr.id = grt.graphic_id)
-        where
-        if(bc.id > 0, bct.schedule, if(de.id > 0, det.schedule, grt.schedule)) = 'open'
-        order by st.name ASC
-        limit 30000"
-        );
     }
 
 }
--- resources/views/historical/index.blade.php
+++ resources/views/historical/index.blade.php
@@ -28,23 +28,50 @@
                             <table class="table table-nomargin table-bordered table-colored-header">
                                 <tbody>
                                     <thead>
-                                        <th>Nome</th>
-                                        <th>Data</th>
-                                        <th>Módulo</th>
-                                        <th>Contato</th>
-                                        <th>Ação</th>
+                                    <tr>
+                                        <th colspan="2">
+                                            <form action="{{route('history.index')}}" method="GET" class="span12" style="margin: 0;padding:0;">
+                                                <div class="span12">
+                                                    <div class="control-group">
+                                                        <div class="controls controls-row">
+                                                            <select name="modulo" id="modulo" class='input-block-level'>
+                                                                <option value="" {{$modulo == '' ? 'selected':''}}>Modulo?</option>
+                                                                <option value="contrato_segunda" {{$modulo == 'contrato_segunda' ? 'selected':''}}>2ª Fase</option>
+                                                                <option value="contrato_terceira" {{$modulo == 'contrato_terceira' ? 'selected':''}}>3ª Fase</option>
+                                                                <option value="cheque" {{$modulo == 'cheque' ? 'selected':''}}>CHEQUE</option>
+                                                                <option value="grafica" {{$modulo == 'grafica' ? 'selected':''}}>GRAFICA</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                        </th>
+                                        <th colspan="1"></th>
+                                        <th colspan="2"></th>
+                                        <th>
+                                                <div class="span12">
+                                                    <div class="control-group">
+                                                        <div class="controls controls-row">
+                                                            <span class="input-group-append">
+                                                                <button type="submit" class="btn btn-sm" style="margin-top:-10px;">Pesquisar</button>
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </th>
+                                    </tr>
+                                        <tr>
+                                            <th>Dt Contato</th>
+                                            <th>Módulo</th>
+                                            <th>Nome</th>
+                                            <th>Dt Retorno</th>
+                                            <th>Observação</th>
+                                            <th>Ação</th>
+                                        </tr>
                                     </thead>
                                     @foreach($historical as $value)
                                         <tr>
-                                            <td>
-                                            @if($value->modulo == 'cheque')
-                                            <a href="{{route('bankCheques.show', ['graphic' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
-                                            @elseif($value->modulo == 'contrato')
-                                            <a href="{{route('defaultings.show', ['defaulting' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
-                                            @else
-                                            <a href="{{route('graphics.show', ['graphic' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
-                                            @endif
-                                            </td>
                                             <td>{{$value->created_at}}</td>
                                             <td>
                                                 @if($value->modulo == 'cheque')
@@ -55,6 +82,16 @@
                                                     Grafica
                                                 @endif
                                             </td>
+                                            <td>
+                                            @if($value->modulo == 'cheque')
+                                            <a href="{{route('bankCheques.show', ['graphic' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
+                                            @elseif($value->modulo == 'contrato')
+                                            <a href="{{route('defaultings.show', ['defaulting' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
+                                            @else
+                                            <a href="{{route('graphics.show', ['graphic' => $value->modulo_id])}}" target="_blank">{{$value->name}}</a>
+                                            @endif
+                                            </td>
+                                            <td>{{$value->dt_retorno}}</td>
                                             <td>{{$value->observacao}}</td>
                                             <td>
                                                 <form action="{{route('history.update')}}" method="POST" onSubmit="return confirm('Deseja encerrar?');" style="padding: 0px;margin:0px;">
