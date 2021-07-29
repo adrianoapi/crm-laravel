c9cf5a8c0321c10defde1ba50d742c0d7faef162
Adriano <adrianoapi@hotmail.com>
2021-07-29 11:10:18 -0300

    update dash

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class HistoryController extends Controller
 {
-    private $title  = 'Retorno';
+    private $title  = 'RETORNO';
 
 
     public function __construct()
