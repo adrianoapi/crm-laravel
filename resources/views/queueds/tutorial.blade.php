0e55261842de32d527f006ac648f9e05f657946d
Adriano Araújo Costa <adrianoapi@hotmail.com>
2023-07-19 15:26:21 -0300

    udpate tutorial

--- resources/views/queueds/tutorial.blade.php
+++ resources/views/queueds/tutorial.blade.php
@@ -9,7 +9,7 @@
         <tr>
             <td>
                 <li>Antes de converter seu excel para csv, certifique-se de que não haja vírgula, ponto e vírgula, apóstrofo ou caracteres especiais, tais como ç, ã, ê,í, ò, ü... </li>
-                <li>Deixe o arquivo com menos de 1000 linhas, se possível divida-o, deixano sempre o cabeçalho em cada um;</li>
+                <li>Deixe o arquivo com no máximo 1000 linhas, se necessário, divida-o em outros arquivos, deixando sempre o cabeçalho em cada um deles;</li>
             </td>
         </tr>
         <tr>
@@ -27,6 +27,18 @@
                 <li>Campo data: pode pode ser formato BR <strong>dd/mm/aaaa</strong> ou formato USA <strong>aaaa-mm-dd</strong>;</li>
                 <li>Campo nome: linhas que não possuem este campo preenchdio, serão ignoradas;</li>
                 <li>Campos monetários: devem ter o formato <strong>0.00</strong>, <strong>0,00</strong> ou apenas <strong>00</strong>;</li>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h5>Possíveis erros</h5>
+                <li>0 linhas registradas: o arquivo está em branco ou não está separado por ponto e vírgula;</li>
+                <li>erro de query:
+                    <ul>
+                        <li>pode está faltando alguma coluna no arquivo, tal como tipo ou fase;</li>
+                        <li>pode haver algum caractere diferente de UTF-8 ou ponto e vígula em algum cadastro, o que faz o sistema entender que tem coluna quebrada;</li>
+                    </ul>
+                </li>
             </td>
         </tr>
         <tr>
